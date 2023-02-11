a2b8010f7f91decc0490a0fbf3a8d48d6fc60270
Larysa33 <ligorev@yahoo.com>
2023-02-11 20:44:36 +0300

    SSQ-14_test_for_sign_up: added the rest of the tests for zipcode field.

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -2,15 +2,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ZipCodeTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless");
+        driver  = new ChromeDriver(options);
+    }
 
     @Test
     public void validZipCode(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver  = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("12345");
@@ -22,8 +32,108 @@ public class ZipCodeTest {
         WebElement registerButton = driver.findElement(By.cssSelector("input[value=Register]")); //можно взять Register в одинарные кавычки
         Assert.assertTrue(registerButton.isDisplayed(),
                 "Register form is not opened ('Register' button is not found)");
-        driver.quit();
     }
 
+    @Test
+    public void emptyZipCode(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
 
+    @Test
+    public void textInputZipCode(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("abcde");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void specialSymbolsInputZipCode(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("*#!?*");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void zipCodeBoundaryTestingNegative(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1234");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void zipCodeBoundaryTestingPositive(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("123456");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void maxNumberOfEnteredDigits(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345123451234512345123451234512345123451234512" +
+                "34512345123451234512345123451234512345123451234512345");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void inputSpaceBeforeZipcode(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys(" 12345");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void inputSpaceInTheMiddle(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("123 45");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @Test
+    public void inputSpaceAfterZipcode(){
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345 ");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(),
+                "Register form is not opened ('Register' button is not found)");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
