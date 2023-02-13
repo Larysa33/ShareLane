f7853b6c3a9b01b3bae4e7e12306affdb41c5d60
Larysa33 <ligorev@yahoo.com>
2023-02-13 23:23:12 +0300

    SSQ-14_test_for_sign_up: code refactoring according pull request comments.

--- src/test/java/signUp/zipcode/ZipcodeTest.java
+++ src/test/java/signUp/zipcode/ZipcodeTest.java
@@ -1,134 +1,61 @@
 package signUp.zipcode;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import util.UtilMethods;
 
-public class ZipcodeTest {
-    WebDriver driver;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-    @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeOptions options=new ChromeOptions();
-        options.addArguments("--headless");
-        driver  = new ChromeDriver(options);
-    }
-
-    @Test
-    public void validZipcodeTest(){
-        boolean isValid=true;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("12345");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();  //можно Continue оставить без кавычек
-        /*альтернатива
-        WebElement continueButton = driver.findElement(By.cssSelector("input[value='Continue']"));
-        continueButton.click();*/
-
-        //можно взять Register в одинарные кавычки
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void emptyZipcodeTest(){
-        boolean isValid=false;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        //можно взять Register в одинарные кавычки
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void textInputZipcodeTest(){
-        boolean isValid=false;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("abcde");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        //можно взять Register в одинарные кавычки
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void specialSymbolsInputZipcodeTest(){
-        boolean isValid=false;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("*#!?*");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        //можно взять Register в одинарные кавычки
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void zipcodeBoundaryTestingNegative(){
-        boolean isValid=false;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("1234");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        //можно взять Register в одинарные кавычки
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void zipcodeBoundaryTestingPositive(){
-        boolean isValid=true;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("123456");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @Test
-    public void maxNumberOfEnteredDigitsTest(){
-        boolean isValid=false;
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
-        zipCodeInput.sendKeys("12345123451234512345123451234512345123451234512" +
-                "34512345123451234512345123451234512345123451234512345");
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value=Register]")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
+public class ZipcodeTest extends BaseTest {
 
     @DataProvider
-    public Object[][] zipcodeValuesWithSpaces() {
+    public Object[][] validZipcodeValues() {
         return new Object[][]{
-                {" 12345", false},
-                {"123 45", false},
-                {"12345 ", false},
+                {"12345"},
+                {"123456"}
         };
     }
 
-    @Test(dataProvider = "zipcodeValuesWithSpaces")
-    public void zipcodeWithSpacesTest(String zipCode, boolean isValid) {
+    @Test(dataProvider = "validZipcodeValues")
+    public void validZipcodeTest(String zipCode) {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys(String.valueOf(zipCode));
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        /* альтернатива
+        WebElement continueButton = driver.findElement(By.cssSelector("input[value='Continue']"));
+        continueButton.click();
+        */
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value=Register]"));
+        Assert.assertTrue(registerButton.isDisplayed(), "Register form is not opened ('Register' button is not found).");
+    }
+
+    @DataProvider
+    public Object[][] invalidZipcodeValues() {
+        return new Object[][]{
+                {""},
+                {"abcde"},
+                {"*#!?."},
+                {"1234"},
+                {"1234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345"}, //fails, должно быть ограничение на ввод символов
+                {" 12345"},
+                {"123 45"},
+                {"12345 "},
+                {" "}
+        };
+    }
+
+    @Test(dataProvider = "invalidZipcodeValues")
+    public void zipcodeNegativeTest(String zipCode) {
         driver.get("https://sharelane.com/cgi-bin/register.py");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys(String.valueOf(zipCode));
-        driver.findElement(By.cssSelector("input[value='Continue']")).click();
-        Assert.assertEquals(UtilMethods.isElementDisplayed(driver, By.cssSelector("input[value='Register']")), isValid,
-                "Register form is not opened ('Register' button is not found)");
-    }
-
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        WebElement errorMessage = driver.findElement(By.className("error_message")); //By.xpath("//*[text()='Oops, error on page. ZIP code should have 5 digits']"))
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not found."); //отваливается для теста на максимальное кол-во цифр
     }
 }
